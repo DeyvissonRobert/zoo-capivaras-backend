67e52085ad8b6128f440ed135f3e174e1ff324e2
DeyvissonRobert <robert.designerx@gmail.com>
2024-09-30 17:29:28 -0300

    Função para buscar capivara

--- src/controllers/capivaraController.js
+++ src/controllers/capivaraController.js
@@ -1,2 +1,12 @@
 const capivaraModel = require('../models/capivaraModel');
 
+exports.getAllCapivaras = (req, res) => {
+    capivaraModel.getAll((err, capivaras) => {
+        if (err) {
+            res.status(500).json({ message: 'Erro ao buscar capivaras' });
+        } else {
+            res.status(200).json(capivaras);
+        }
+    });
+};
+
