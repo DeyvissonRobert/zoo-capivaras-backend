376288cff80fca6254c6c532461faf2822b5f61c
DeyvissonRobert <robert.designerx@gmail.com>
2024-09-30 17:13:41 -0300

    Criando conexão com o db

--- config/db.js
+++ config/db.js
@@ -1,2 +1,18 @@
 const mysql = require('mysql2');
 
+const connection = mysql.createConnection({
+    host: 'localhost',  
+    user: 'root',       
+    password: '40028922', 
+    database: 'capivaras_db' 
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.error('Erro ao conectar ao banco de dados:', err);
+    } else {
+        console.log('Conectado ao banco de dados MySQL');
+    }
+});
+
+module.exports = connection;
