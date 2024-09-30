97fa5f1c5da264d7182e2dd581bdb83c934af0a4
DeyvissonRobert <robert.designerx@gmail.com>
2024-09-30 17:16:46 -0300

    Configuração do arquivo principal e Gerando rotas

--- src/app.js
+++ src/app.js
@@ -1,7 +1,17 @@
 const express = require('express');
+const capivaraController = require('./controllers/capivaraController');
 
 const app = express();
+
+
 app.use(express.json());
 
+app.get('/capivaras', capivaraController.getAllCapivaras);
+app.post('/capivaras', capivaraController.createCapivara);
+app.put('/capivaras/:id', capivaraController.updateCapivara);
+app.delete('/capivaras/:id', capivaraController.deleteCapivara);
 
-
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
