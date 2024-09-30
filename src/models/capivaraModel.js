aa3b0bf34c95eb989506061d8536c5b954dd3fc3
DeyvissonRobert <robert.designerx@gmail.com>
2024-09-30 18:00:26 -0300

    Função para buscar todas as capivaras

--- src/models/capivaraModel.js
+++ src/models/capivaraModel.js
@@ -1,2 +1,13 @@
 const db = require('../../config/db');
 
+exports.getAll = (callback) => {
+    const sql = 'SELECT * FROM capivaras';
+    db.query(sql, (err, results) => {
+        if (err) {
+            callback(err, null);
+        } else {
+            callback(null, results);
+        }
+    });
+};
+
