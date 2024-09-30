954cc7809de8abc271639ecea4b432ec64fb498d
DeyvissonRobert <robert.designerx@gmail.com>
2024-09-30 17:18:39 -0300

    Instalando cors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const cors = require('cors'); 
 const capivaraController = require('./controllers/capivaraController');
 
 const app = express();
 
-
+app.use(cors());
 app.use(express.json());
 
 app.get('/capivaras', capivaraController.getAllCapivaras);
